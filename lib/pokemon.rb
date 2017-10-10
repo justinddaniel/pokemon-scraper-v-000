6b57415a283605924a76927998b2cd64889ec16f
Justin Daniel <justinddaniel@gmail.com>
2017-10-10 22:26:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,13 @@
 class Pokemon
 
   def initialize(id, name, type, db)
-    @id = id 
-    @name = name 
+    @id = id
+    @name = name
     @type = type
     @db = db
   end
-  
+
+  def self.save(name, type, @db)
+    @db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type, @db)
+  end
 end
